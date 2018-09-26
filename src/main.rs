cf2ba9942346bb778e477fbd90e8f520de364c7f
Marco Luglio <marcodejulho@gmail.com>
2018-09-26 17:50:16 -0300

    Remoção de comentário não relevante

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,6 @@ use parser::Lexer; // tem que importar toda a vez que for usar JsLexer?? Aff..
 
 fn main() {
 
-	/*
-	let js_lexer = parser::JsLexer{
-		token_pool: vec![] // FIXME ver como colocar isso no construtor do Lexer quem sabe
-	};
-	*/
 	let mut js_lexer:parser::JsLexer = Default::default();
 	let tokens = js_lexer.parse("let a = 'a';");
 	println!("output:");
